2f4dd6baa9bd4a3a6399896e3ff0c1aa1b154888
Crowdbotics <engineering@crowdbotics.com>
2023-05-14 23:21:22 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/super_bread_dev_83155/MainActivity.java
@@ -0,0 +1,15 @@
+package com.super_bread_dev_83155;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "super_bread_dev_83155";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "super-bread-dev-83155",
+  url: "https://super-bread-dev-83155.botics.co",
+  api: "https://super-bread-dev-83155.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
